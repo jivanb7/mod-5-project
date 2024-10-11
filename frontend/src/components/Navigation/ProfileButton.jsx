93b3090c7057432e45e59ee7cf132a4c7aac8f8a
jivanb7 <bal.jivan@yahoo.com>
2024-10-11 14:23:16 -0700

    phase 3 buttons half way

---
+++ frontend/src/components/Navigation/ProfileButton.jsx
@@ -0,0 +1,12 @@
+
+import { GoArchive } from "react-icons/go";
+
+const ProfileButton = () => {
+  return (
+    <div style={{ color: "white", fontSize: "100px" }}>
+      <GoArchive />
+    </div>
+  );
+};
+
+export default ProfileButton;
