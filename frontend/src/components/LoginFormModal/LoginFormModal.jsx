7659022cc180952ae358001aa4f6860330b05d03
jivanb7 <bal.jivan@yahoo.com>
2024-10-14 09:44:37 -0700

    login feature okay

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -1,6 +1,6 @@
 // frontend/src/components/LoginFormModal/LoginFormModal.jsx
 
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import * as sessionActions from '../../store/session';
 import { useDispatch } from 'react-redux';
 import { useModal } from '../../context/Modal';
@@ -12,6 +12,15 @@ function LoginFormModal() {
   const [password, setPassword] = useState("");
   const [errors, setErrors] = useState({});
   const { closeModal } = useModal();
+
+  // Reset form fields and errors when modal is closed
+  useEffect(() => {
+    return () => {
+      setCredential("");
+      setPassword("");
+      setErrors({});
+    };
+  }, []);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -49,10 +58,27 @@ function LoginFormModal() {
           />
         </label>
         {errors.credential && (
-          <p>{errors.credential}</p>
+          <p>{errors.credential === 'Invalid credentials'
+            ? 'The provided credentials were invalid' 
+            : errors.credential}
+          </p>
         )}
-        <button type="submit">Log In</button>
+        <button
+          type="submit"
+          disabled={credential.length < 4 || password.length < 6}  
+        >Log In
+        </button>
       </form>
+      
+      <button
+        type="button"
+        onClick={() =>
+          dispatch(sessionActions.login({ credential: 'demo@user.io', password: 'password' }))
+          .then(closeModal)
+        }
+      >
+        Log in as Demo User
+      </button>
     </>
   );
 }
