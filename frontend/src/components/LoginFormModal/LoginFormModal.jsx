5d021a1a75875c3cd09619ded6afcaa1ea2b3506
jivanb7 <bal.jivan@yahoo.com>
2024-10-16 23:44:12 -0700

    log in error successful

--- frontend/src/components/LoginFormModal/LoginFormModal.jsx
+++ frontend/src/components/LoginFormModal/LoginFormModal.jsx
@@ -29,7 +29,6 @@ function LoginFormModal() {
       .then(closeModal)
       .catch(async (res) => {
         const data = await res.json();
-        console.log("INSPECT DATA : ", data);
         if (data && data.errors) {
           setErrors(data.errors);
         } else {
